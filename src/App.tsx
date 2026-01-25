442800f13fb7591b5a93bc98822b6a44a74138fe
Raj Kale <rajramnathkale841@gmail.com>
2026-01-25 16:04:20 +0530

    Implement code for the About tab

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
+import { useState } from 'react';
+import About from './components/About';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 import Stats from './components/stats';
 import Timeline from './components/TimeLine';
 
 export default function App() {
+  const [activeSection, setActiveSection] = useState<'home' | 'about'>('home');
+
   return (
     <div className="bg-[#0b0b0b] text-white min-h-screen">
-      <Navbar />
-      <Hero />
-      <Stats />
-      <Timeline />
+      <Navbar setActiveSection={setActiveSection} />
+
+      {activeSection === 'home' && (
+        <>
+          <Hero />
+          <Stats />
+          <Timeline />
+        </>
+      )}
+
+      {activeSection === 'about' && <About />}
     </div>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,31 @@
-export default function Navbar() {
+type Props = {
+  setActiveSection: (section: 'home' | 'about') => void;
+};
+
+export default function Navbar({ setActiveSection }: Props) {
   return (
-    <nav className="flex items-center justify-between px-10 py-6">
-      <h1 className="text-3xl font-bold text-lime-400">RK</h1>
+    <nav className="w-full flex items-center justify-between px-20 py-6">
+      <h1 className="text-2xl font-bold text-lime-400">RK</h1>
 
       <ul className="flex gap-8 text-sm text-gray-300">
-        <li className="text-lime-400">Home</li>
-        <li>About</li>
-        <li>Services</li>
-        <li>Projects</li>
-        <li>Contact</li>
+        <li
+          onClick={() => setActiveSection('home')}
+          className="cursor-pointer hover:text-lime-400"
+        >
+          Home
+        </li>
+        <li
+          onClick={() => setActiveSection('about')}
+          className="cursor-pointer hover:text-lime-400"
+        >
+          About
+        </li>
+        <li className="cursor-pointer hover:text-lime-400">Services</li>
+        <li className="cursor-pointer hover:text-lime-400">Projects</li>
+        <li className="cursor-pointer hover:text-lime-400">Contact</li>
       </ul>
 
-      <button className="bg-lime-400 text-black px-5 py-2 rounded-full font-semibold hover:scale-105 transition">
+      <button className="bg-lime-400 text-black px-5 py-2 rounded-full font-semibold">
         Download CV
       </button>
     </nav>
