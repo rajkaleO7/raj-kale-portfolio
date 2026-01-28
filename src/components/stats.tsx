241d5ac52e03306712f9d0b4d6129342b0b81e35
Raj Kale <rajramnathkale841@gmail.com>
2026-01-28 10:35:13 +0530

    update projects count

--- src/components/stats.tsx
+++ src/components/stats.tsx
@@ -2,7 +2,7 @@ export default function Stats() {
   return (
     <section className="grid grid-cols-4 gap-10 px-16 py-10 text-center">
       {[
-        { num: '3+', label: 'Projects' },
+        { num: '5+', label: 'Projects' },
         { num: '2+', label: 'Years Learning' },
         { num: '5+', label: 'Technologies' },
         { num: '100%', label: 'Dedication' },
